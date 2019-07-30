7e48390bb3d43a57c8b404c8bbf33ec56e66f7e2
Erika Martinez <evmartinez@gmail.com>
2019-07-29 20:27:35 -0700

    removed and added some code

--- script.py
+++ script.py
@@ -1,23 +1,22 @@
-import math
-import sys
-from os import rename
+# import math
+# import sys
+# from os import rename
 
 import requests
 
-# code-runner run button at top right can be used for this type of code
-# However, to run input/output code you can't use that button. Refer to
-# script2.py file for an example.
+# # code-runner run button at top right can be used for this type of code
+# # However, to run input/output code you can't use that button. Refer to
+# # script2.py file for an example.
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
-# print(greet('World'))
-# print(greet('Monkey'))
+print(r.ok)
