5bdd8c704740d75d6ce1f9e0e1ff3e82c6a1a71f
Erika Martinez <evmartinez@gmail.com>
2019-07-29 20:24:36 -0700

    initial commit

---
+++ script.py
@@ -0,0 +1,23 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# code-runner run button at top right can be used for this type of code
+# However, to run input/output code you can't use that button. Refer to
+# script2.py file for an example.
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+# print(greet('World'))
+# print(greet('Monkey'))
---
+++ script2.py
@@ -0,0 +1,11 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+# To run input/output code you can't use code-runner,
+# so right-click and Run in python terminal
+
+name = input("Your name? ")
+print("Hello,", name)
